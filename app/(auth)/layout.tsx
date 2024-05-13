0fde733efccf357f01e95b1519a0f6353bc6d86d
ryan <58205665+ryanpaul484@users.noreply.github.com>
2024-05-13 10:46:21 +0800

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+
+
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata  = {
+  title: "Creative Gallery",
+  description: "A Next.js 13 Meta Threads Application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+          {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
